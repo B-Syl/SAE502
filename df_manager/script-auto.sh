34b020b7b4147b8dc43ebb5738d6c42c4bb3caaa
B-Syl <sylvain.bouliteau@etu.univ-st-etienne.fr>
2024-03-30 11:35:53 +0100

    Modif 30-03-24

--- df_manager/script-auto.sh
+++ df_manager/script-auto.sh
@@ -2,3 +2,4 @@
 ansible-playbook -i inventaire.ini --user root --private-key ansible-man-key deploy-lamp.yml
 sshpass -f rootpassword ssh-copy-id -i ansible-man-key.pub root@ctsite
 sshpass -f rootpassword ssh-copy-id -i ansible-man-key.pub root@ctbackup
+ansible-playbook -i inventaire.ini --user root --private-key ansible-man-key install-lamp.yml
